8f95b88a5189eb68dd275e65002b0f84697a0060
huaoweiomian <huaoweiomian@163.com>
2020-05-05 20:02:36 +0800

    set db tbl start index to 1

--- tbls.sql
+++ tbls.sql
@@ -1,3 +1,3 @@
 CREATE TABLE `history` (  `id` bigint(20) NOT NULL AUTO_INCREMENT,  `name` text CHARACTER SET latin1,  `path` text CHARACTER SET latin1,  PRIMARY KEY (`id`)) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
-CREATE TABLE `usr` (  `id` bigint(20) NOT NULL AUTO_INCREMENT,  `name` text CHARACTER SET latin1,  `pwd` text CHARACTER SET latin1,  `admin` int(11) DEFAULT NULL COMMENT '0:不是admin,1:是admin',  PRIMARY KEY (`id`)) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+CREATE TABLE `usr` (  `id` bigint(20) NOT NULL AUTO_INCREMENT,  `name` text CHARACTER SET latin1,  `pwd` text CHARACTER SET latin1,  `admin` int(11) DEFAULT NULL COMMENT '0:不是admin,1:是admin',  PRIMARY KEY (`id`)) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
